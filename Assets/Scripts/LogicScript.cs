80929da67cbf77f9b1e12ca078365250f815f6ff
jonathanhj123 <jona246@gmail.com>
2025-11-24 17:06:18 +0100

    xD

--- Assets/Scripts/LogicScript.cs
+++ Assets/Scripts/LogicScript.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class LogicScript : MonoBehaviour
 {
 
     private BoxCollider2D door;
+    public UIDocument UI;
+    private Label keyLabel;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         door = GameObject.FindGameObjectWithTag("Door").GetComponent<BoxCollider2D>();
+        keyLabel = GetComponent().rootVisualElement.Q(key);
     }
 
     // Update is called once per frame
@@ -18,5 +23,6 @@ public class LogicScript : MonoBehaviour
     public void keyEnter()
     {
         door.isTrigger = true;
+        keyLabel.tintColor =  new Color(255f, 255f, 255f, 255f);
     }
 }
